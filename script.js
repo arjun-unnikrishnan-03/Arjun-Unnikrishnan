1bb7dafcfaead50862f44e0185b2a6d663c6a07b
arjun-unnikrishnan-03 <151398289+arjun-unnikrishnan-03@users.noreply.github.com>
2025-07-14 19:28:42 +0530

    Enhanced UI: contrast, overlay, responsive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function copyEmail(event) {
   event.preventDefault();
-  navigator.clipboard.writeText("arjun@example.com").then(() => {
+  navigator.clipboard.writeText("arjun.u1968@gmail.com.com").then(() => {
     alert("Email copied to clipboard!");
   });
 }
