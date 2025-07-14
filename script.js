8849c2287da9248319dca9e8dc2a7d32b2e44d4b
arjun-unnikrishnan-03 <151398289+arjun-unnikrishnan-03@users.noreply.github.com>
2025-07-14 19:44:39 +0530

    Enhanced UI: contrast, overlay, responsive

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function copyEmail(event) {
     alert("Email copied to clipboard!");
   });
 }
+const textArray = ["AI Enthusiast", "Software Developer", "Creative Thinker"];
+let i = 0, j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+  if (i < textArray.length) {
+    if (!isDeleting && j <= textArray[i].length) {
+      currentText = textArray[i].substring(0, j++);
+    } else if (isDeleting && j >= 0) {
+      currentText = textArray[i].substring(0, j--);
+    }
+
+    document.querySelector(".typewriter-text").innerHTML = currentText + "|";
+
+    if (j === textArray[i].length) {
+      isDeleting = true;
+      setTimeout(type, 1000);
+    } else if (j === 0 && isDeleting) {
+      isDeleting = false;
+      i = (i + 1) % textArray.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+  }
+}
+window.onload = type;
+
